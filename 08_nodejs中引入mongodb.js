2c46bb3b447f8c33466659bcd83e44c2c4dd18a5
liuzhihua <liuzhihua@lushangchuangfu.com>
2019-01-11 14:41:35 +0800

    连接数据库插入数据成功，成功拿到db.close函数

--- 08_nodejs中引入mongodb.js
+++ 08_nodejs中引入mongodb.js
@@ -9,6 +9,8 @@ var MongoClient = require("mongodb").MongoClient;
 var mongodbUrl = "mongodb://localhost:27017";
 var dbName = "user";
 
+
+let number = 0;
 var server = http.createServer(function (req, res) {
     var pathname = url.parse(req.url, true).pathname;
     if (pathname == "/") {
@@ -31,6 +33,7 @@ var server = http.createServer(function (req, res) {
 
     // 新增数据
 
+
     if (pathname == "/add.html") {
         // 4、nodejs连接数据库
         MongoClient.connect(mongodbUrl, { useNewUrlParser: true }, function (err, client) {
@@ -43,6 +46,38 @@ var server = http.createServer(function (req, res) {
             }
             res.writeHead(200, { "Content-Type": "text/html;charset='utf-8'" })
             // res.write('<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>')
+
+            // 5、操作数据库
+            // let db = client.db(dbName); //数据库db对象
+            MongoClient.connect(mongodbUrl, { useNewUrlParser: true }, function (err, db) {
+                if (err) {
+                    res.end("内部db失败");
+                    return;
+                }
+
+                //尝试 3.0 写法 成功
+                // const mydb = db.db('需要引用的数据库名');
+                // mydb.collection(colleactionsName);
+                let mydb = db.db("user");
+
+                mydb.collection("teacher").insertOne({ "name": "张三" + number }, function (err, result) {
+                    if (err) {
+                        res.end("db fail");
+                        return;
+                    }
+                    number++;
+                    db.close();//关闭连接
+                    console.log("=========================")
+                    console.log(db.close)
+                    console.log("=========================")
+                    // console.log(db)
+                    console.log("=========================")
+                    console.log(result.result)
+                    res.end("db已关闭")
+                })
+            })
+
+
             res.end("success")
         })
     }

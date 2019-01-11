a10b48fb004958a5a2a2c336a8b6a2bc2d857c1e
liuzhihua <liuzhihua@lushangchuangfu.com>
2019-01-11 20:22:02 +0800

    列表接口

--- 10_mongodb查找数据.js
+++ 10_mongodb查找数据.js
@@ -9,6 +9,8 @@ var dbUrl = "mongodb://localhost:27017";
 let number = 0;
 var server = http.createServer(function (req, res) {
   var pathname = url.parse(req.url).pathname;
+
+  // 添加数据
   if (pathname == "/add.html") {
     MongoClient.connect(dbUrl, { useNewUrlParser: true }, function (err, db) {
       if (err) {
@@ -31,5 +33,42 @@ var server = http.createServer(function (req, res) {
 
     })
   }
+
+
+  //查找数据
+  if (pathname == "/list.html") {
+
+    MongoClient.connect(dbUrl, { useNewUrlParser: true }, function (err, db) {
+      if (err) {
+        console.log("查询数据连接mongodb失败");
+        return;
+      }
+
+      var myDb = db.db("user");
+      var result = myDb.collection("dancer").find({});
+      console.log(result)
+
+      let list = [];
+
+      result.each(function (err, doc) {
+        if (err) {
+          console.log("result失败");
+          return;
+        }
+
+        if (doc != null) {
+          list.push(doc); //一条一条将数据插入到list数组中
+        } else {
+          res.end(list)
+          db.close();
+        }
+
+      })
+
+
+    })
+  }
+
+
 
 }).listen(8889);

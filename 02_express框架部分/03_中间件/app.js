c81b29154936df22fb58830d463d66d1a057989c
liuzhihua <liuzhihua@lushangchuangfu.com>
2019-01-16 20:02:58 +0800

    加密存取cookie

--- 02_express框架部分/03_中间件/app.js
+++ 02_express框架部分/03_中间件/app.js
@@ -1,27 +1,27 @@
 let express = require("express");
 let app = new express();
 
-//1、 安装cookie-parser     npm install cookie-parser --save
-
-// 引入
 let cookieParser = require("cookie-parser");
+app.use(cookieParser("13245679679"));
 
-// 使用cookieParser中间件
-app.use(cookieParser());
-
-
-app.get("/index", function (req, res) {
-    console.log(req.cookies);
-    res.send(req.cookies);
+app.get("/", function (req, res) {
+    res.send("index")
 })
 
-app.get("/setcookie", function (req, res) {
-//  设置cookie ,如果还在有效时间内，关闭浏览器cookie是不会失效的 maxAge多久后失效
-    res.cookie("cookieName", "zhangsan", {maxAge: 600000});
-    res.send("设置cookie成功");
-
-
+app.get("/get", function (req, res) {
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
 })
 
+app.get("/set", function (req, res) {
+    // maxAge:时效性毫秒
+    // path限定只有/get路由下才能获取cookie
+    //signed:true,存储时加密存储
+    res.cookie("roseCookie", "rose", {maxAge: 300000, path: "/get", signed: true});
+    res.send("设置cookie成功")
+})
 
-app.listen(8888, "127.0.0.1")
+app.listen(8888, "localhost");
+
+
+

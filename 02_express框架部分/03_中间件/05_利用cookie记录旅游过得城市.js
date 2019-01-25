3dfe834a00b66640e81a94d55bce41e1015bfb9c
liuzhihua <liuzhihua@lushangchuangfu.com>
2019-01-25 15:24:23 +0800

    05_利用cookie记录旅游过得城市.js

--- 02_express框架部分/03_中间件/05_利用cookie记录旅游过得城市.js
+++ 02_express框架部分/03_中间件/05_利用cookie记录旅游过得城市.js
@@ -11,21 +11,22 @@ app.get("/", function (req, res) {
 
 app.get("/set", function (req, res) {
 	let citys = req.signedCookies.citys;
-	// console.log(citys)
 	// 用户没有在路径中输入 ?luyou=城市字样，拿不到参数
 	if (citys) {
 		if (!req.query.luyou) {
 			res.send("有去过的城市记录--路由中没有输入新的城市---" + citys);
 			return
 		} else {
-			citys.push(req.query.luyou); //更新去过的城市
+
+			if (citys.indexOf(req.query.luyou) == -1) { //去重
+				citys.push(req.query.luyou); //更新去过的城市
+			}
+
 			res.cookie("citys", citys, { maxAge: 60000, signed: true }) //更新存储cookie
 			res.send("有去过的城市记录--路由有输入新的城市" + citys)
 		}
 	} else {
 		if (!req.query.luyou) {
-			// citys = [];
-			// res.cookie("citys", citys, { maxAge: 60000, signed: true }) //更新存储cookie
 			res.send("ji有去过的城市记录，ye没有输入路由城市");
 		} else {
 			citys = [];
@@ -34,23 +35,6 @@ app.get("/set", function (req, res) {
 			res.send("mei有去过的城市记录--路由有输入新的城市" + citys)
 		}
 	}
-
-
-	// if (citys) {
-	// 	citys.push(req.query.luyou)
-	// } else {
-	// 	citys = [];
-	// 	// 防止没传参的时候拿到的是undefined,防止之前访问过了存储重复的数据
-	// 	if (req.query.luyou) {
-	// 		citys.push(req.query.luyou);
-	// 	}
-	// }
-
-	// console.log(citys)
-
-	// 用cookie存储起来
-	// res.cookie("citys", citys, { maxAge: 60000, signed: true });
-	// res.send("要去的城市为：" + req.query.luyou)
 })
 
 app.listen(8888, "localhost")

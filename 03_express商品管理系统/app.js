06c05214b327fe892ba9d94ba8dddbedee404270
liuzhihua <liuzhihua@lushangchuangfu.com>
2019-01-31 14:41:29 +0800

    配置登录页和edit编辑页面的模板引擎

--- 03_express商品管理系统/app.js
+++ 03_express商品管理系统/app.js
@@ -1,20 +1,25 @@
 let express = require("express");
 let app = new express();
 
+app.use(express.static("public")); //配置静态文件
+
+app.set("view engine", "ejs");//设置ejs引擎
+
 app.get("/", function (req, res) {
-  res.send("login")
+  res.render("login")
 })
 app.get("/product", function (req, res) {
-  res.send("product")
+  res.render("product")
 })
 app.get("/productadd", function (req, res) {
-  res.send("productadd")
+  res.render("productadd")
 })
 app.get("/productedit", function (req, res) {
-  res.send("productedit")
+  res.render("productedit")
 })
 app.get("/productdelete", function (req, res) {
-  res.send("productdelete")
+  // res.render("productdelete")
+  res,send("productdelete")
 })
 
 app.listen(8888, "127.0.0.1")
